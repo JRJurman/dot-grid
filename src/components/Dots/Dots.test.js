8c0506fab1f9d4dcf481ce0df4a4c561a9ffb049
JRJurman <jrjurman@gmail.com>
2019-08-02 23:27:29 -0400

    tests for Dots

--- src/components/Dots/Dots.test.js
+++ src/components/Dots/Dots.test.js
@@ -1,7 +1,26 @@
 import Dots from './Dots'
+import { useUrlParams } from 'tram-one'
+
+jest.mock('tram-one', () => ({
+  ...(jest.requireActual('tram-one')),
+  useUrlParams: jest.fn()
+}));
 
 describe('Dots', () => {
   describe('with default props', () => {
+    useUrlParams.mockImplementation(() => ({}));
+    const wrapper = Dots()
+
+    it('should match snapshot', () => {
+      expect(wrapper.outerHTML).toMatchSnapshot()
+    })
+  })
+
+  describe('with configured number of dots', () => {
+    useUrlParams.mockImplementation(() => ({
+      numberOfDots: '4'
+    }));
+
     const wrapper = Dots()
 
     it('should match snapshot', () => {
